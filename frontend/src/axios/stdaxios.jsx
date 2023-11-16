dde14d54934325340bba160ba6d647130bd1c8c7
Hannah <hannaelsajoy@gmail.com>
2023-11-16 19:22:04 +0530

    http updates

--- frontend/src/axios/stdaxios.jsx
+++ frontend/src/axios/stdaxios.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 
 const stdaxiosInstance=axios.create(
     {
-        baseURL:"http://127.0.0.1:8000/std/",
+        baseURL:"http://psykicks.social/std/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
--- frontend/src/axios/tutoraxios.jsx
+++ frontend/src/axios/tutoraxios.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 
 const tutoraxiosInstance=axios.create(
     {
-        baseURL:"http://127.0.0.1:8000/tutor/",
+        baseURL:"http://psykicks.social/tutor/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
