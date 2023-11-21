53681d60ad23ad4155e57b80aa9cb4566cd8f084
Hannah <hannaelsajoy@gmail.com>
2023-11-21 12:22:33 +0530

    axios updated

--- frontend/src/axios/stdaxios.jsx
+++ frontend/src/axios/stdaxios.jsx
@@ -3,7 +3,8 @@ import axios from "axios"
 
 const stdaxiosInstance=axios.create(
     {
-        baseURL:"http://127.0.0.1:8000/std/",
+        // baseURL:"http://127.0.0.1:8000/std/",
+        baseURL:"http://psykicks.social/std/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
