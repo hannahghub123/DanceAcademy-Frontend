239799519dad9993d53369468879e9b0c9046e19
Hannah <hannaelsajoy@gmail.com>
2023-11-23 23:08:14 +0530

    bugs resolving 5th commit

--- frontend/src/axios/tutoraxios.jsx
+++ frontend/src/axios/tutoraxios.jsx
@@ -3,8 +3,8 @@ import axios from "axios"
 
 const tutoraxiosInstance=axios.create(
     {
-        baseURL:"http://127.0.0.1:8000/tutor/",
-        // baseURL:"https://psykicks.social/tutor/",
+        // baseURL:"http://127.0.0.1:8000/tutor/",
+        baseURL:"https://psykicks.social/tutor/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
