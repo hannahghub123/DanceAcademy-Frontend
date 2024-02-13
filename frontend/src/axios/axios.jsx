eb458f37b55dc9e12bdd2cbaa995f16d63109ebd
Hannah <hannaelsajoy@gmail.com>
2024-02-13 12:11:12 +0530

    axios updated

--- frontend/src/axios/axios.jsx
+++ frontend/src/axios/axios.jsx
@@ -5,7 +5,7 @@ const axiosInstance=axios.create(
     {
         // baseURL:"http://127.0.0.1:8000/academy-admin/",
         // baseURL:"https://psykicks.social/academy-admin/",
-        baseURL:"https://danceacademy-backend.onrender/academy-admin/",
+        baseURL:"https://danceacademy-backend.onrender.com/academy-admin/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
