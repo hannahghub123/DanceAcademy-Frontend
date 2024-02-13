e69fe29e3d9cc48dcfe681f0587e58ff801415f6
Hannah <hannaelsajoy@gmail.com>
2024-02-13 11:58:57 +0530

    axios edits- for depolying

--- frontend/src/axios/stdaxios.jsx
+++ frontend/src/axios/stdaxios.jsx
@@ -4,7 +4,8 @@ import axios from "axios"
 const stdaxiosInstance=axios.create(
     {
         // baseURL:"http://127.0.0.1:8000/std/",
-        baseURL:"https://psykicks.social/std/",
+        // baseURL:"https://psykicks.social/std/",
+        baseURL:"https://danceacademy-backend.onrender/academy-admin/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
--- frontend/src/axios/tutoraxios.jsx
+++ frontend/src/axios/tutoraxios.jsx
@@ -4,7 +4,8 @@ import axios from "axios"
 const tutoraxiosInstance=axios.create(
     {
         // baseURL:"http://127.0.0.1:8000/tutor/",
-        baseURL:"https://psykicks.social/tutor/",
+        // baseURL:"https://psykicks.social/tutor/",
+        baseURL:"https://danceacademy-backend.onrender/academy-admin/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
