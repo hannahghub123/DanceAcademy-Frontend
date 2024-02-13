64afd958ce1e1790d67a9e7c0852552240167521
Hannah <hannaelsajoy@gmail.com>
2024-02-13 12:24:43 +0530

    axios-url updated

--- frontend/src/axios/stdaxios.jsx
+++ frontend/src/axios/stdaxios.jsx
@@ -5,7 +5,7 @@ const stdaxiosInstance=axios.create(
     {
         // baseURL:"http://127.0.0.1:8000/std/",
         // baseURL:"https://psykicks.social/std/",
-        baseURL:"https://danceacademy-backend.onrender.com/academy-admin/",
+        baseURL:"https://danceacademy-backend.onrender.com/std/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
--- frontend/src/axios/tutoraxios.jsx
+++ frontend/src/axios/tutoraxios.jsx
@@ -5,7 +5,7 @@ const tutoraxiosInstance=axios.create(
     {
         // baseURL:"http://127.0.0.1:8000/tutor/",
         // baseURL:"https://psykicks.social/tutor/",
-        baseURL:"https://danceacademy-backend.onrender.com/academy-admin/",
+        baseURL:"https://danceacademy-backend.onrender.com/tutor/",
         headers:{
             'Content-Type':'application/json',
             // Authorization:localStorage.getItem("authToken")? "Bearer " + JSON.parse(localStorage.getItem("authToken")):null,
